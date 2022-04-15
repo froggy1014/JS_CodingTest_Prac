041620fe4dc74f7ed0c2049cb1ccfa49413639d8
JeongMin Lee <76844856+froggy1014@users.noreply.github.com>
2022-04-15 10:33:36 +0900

    1_5 done.
    Math.min() Method to get min val of array

--- chapter1/1_5.js
+++ chapter1/1_5.js
@@ -1,11 +1,23 @@
 function solution(arr){
-    let answer, min = Number.MAX_SAFE_INTEGER;
 
-    for(let i=0; i<arr.length;i++){
-        if (arr[i] < min)
-            min = arr[i];
-    } 
-    answer = min;
+ // Math.min 이라는 메소드를 통해서 최솟값을 구할 수 있다. 
+ // 직접 배열 값을 넣어주거나 배열 변수를 넣어주려면 
+ // 전개연산자(Spread Operator)를 넣어주면 사용 가능하다   
+let answer = Math.min(5,7,1,3,2,9,11);
+console.log(answer);
+answer = Math.min(arr);
+console.log(answer);
+answer = Math.min(...arr);
+console.log(answer);
+
+
+    // let answer, min = Number.MAX_SAFE_INTEGER;
+
+    // for(let i=0; i<arr.length;i++){
+    //     if (arr[i] < min)
+    //         min = arr[i];
+    // } 
+    // answer = min;
     return answer;
 }
 
