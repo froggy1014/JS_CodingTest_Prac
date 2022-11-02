5d3db4a0170d086f328d072572b43ec26372726b
froggy1014 <clcl6084@gmail.com>
2022-11-02 16:40:15 +0900

    BaekJoon[수정렬2]: 문제 해결 및 정리

--- BaekJoon/정렬/수정렬2/app.js
+++ BaekJoon/정렬/수정렬2/app.js
@@ -1,16 +1,42 @@
-const fs=require('fs');
-const input=fs.readFileSync('./input.txt').toString().trim().split('\n').map(Number);
+let input = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
 
-const [ M, ...N ] = input;
+input.shift();
 
-let Array = N;
+let answer = input.sort((a,b) => a - b);
 
-for(let i = 0 ; i < M; i++){
-  for(let j = 0; j < M - i; j++ ) {
-    if(Array[j] > Array[j+1])[Array[j],Array[j+1]] = [Array[j+1],Array[j]]
-  }
-}
- 
-for( let i = 0; i < M ; i++){
-  console.log(Array[i]);
-}
+console.log(answer.join('\n'));
+
+// 선택정렬 
+// for( let i = 0; i < max-1; i++) {
+//   let idx = i;
+//   for( let j = i+1; j < max; j++){
+//     if(numArray[idx] > numArray[j]) idx=j;
+//   }
+//   [numArray[i],numArray[idx]]=[numArray[idx],numArray[i]];
+//   console.log(numArray[i]);
+// }
+// console.log(numArray[max-1]);
+
+// 버블정렬
+// for( let i = 0 ; i < max - 1; i++){
+//   for(let j = 0; j < max - i - 1; j++){
+//     if( numArray[j] > numArray[j+1] ) 
+//       [numArray[j],numArray[j+1]] = [numArray[j+1],numArray[j]] 
+//   }
+// }
+// console.log(numArray);
+
+
+// 선택정렬
+// for ( let i = 0 ; i < max ; i++) {
+//   let temp = numArray[i];
+//   let j;
+//   for (j = i-1; j >= 0; j--) {
+//     if(numArray[j] > temp) numArray[j+1] = numArray[j];
+//     else break;
+//   }
+//   numArray[j+1] = temp;
+// }
+// numArray.map(( v ) => console.log(v));
+
+// numArray.sort().map((v) => console.log(v));
