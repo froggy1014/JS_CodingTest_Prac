de79e6a218a534951df4fad44f1a10f156a56688
froggy1014 <clcl6084@gmail.com>
2022-11-18 18:37:15 +0900

    BaekJoon[체스판 채우기]: 문제 정리

--- BaekJoon/브루트포스/체스판 다시 칠하기/app.js
+++ BaekJoon/브루트포스/체스판 다시 칠하기/app.js
@@ -1,43 +1,56 @@
 const fs=require('fs');
 const input=fs.readFileSync('./input.txt').toString().trim().split('\n');
 
-const [L, R] = input.shift().split(' ');
+const [L, R] = input.shift().split(' ').map(Number);
 
-const HALF = 32;
-let MIN = Number.MAX_SAFE_INTEGER;
-console.log(L,R)
-if( L + R === 16) {
-  let acc = 0;
-  input.forEach((v) => {
-    acc += v.split('').filter((v) => v !== 'W').length
-  })
-  if ( acc > HALF ) MIN = Math.min(MIN, acc - HALF)
-  if ( acc < HALF ) MIN = Math.min(MIN, HALF - acc)
-  return console.log(MIN);
+let MIN = 64;
+
+const whiteFirstBoard = [
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+];
+
+const blackFirstBoard = [
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+  "BWBWBWBW",
+  "WBWBWBWB",
+];
+
+function compare(height,width) {
+  let candi1 = 0;
+  let candi2 = 0;
+  let temph = 0;
+  for( let h = height ; h < height + 8; h++) {
+    let tempw = 0;
+    for( let w = width ; w < width + 8; w++) {
+      if(input[h][w] !== blackFirstBoard[temph][tempw]) candi1++;
+      if(input[h][w] !== whiteFirstBoard[temph][tempw]) candi2++;
+      tempw++;
+    }
+    temph++;
+  }
+  return Math.min(candi1, candi2);
 }
 
-for(let h = 0 ; h <= L - 8 ; h++){
-  for( let w = 0 ; w <= R - 8 ; w++){
-    let white = 0;
-    let black = 0;
-    for( let j = w ; j < 8 ; j++) {
-      black += input[j].split('').filter((v) => v !== 'W').length
-      white += input[j].split('').filter((v) => v !== 'B').length
-    }
-
-    if( black < white) {
-      if ( black > HALF ) MIN = Math.min(MIN, black - HALF)
-      if ( black < HALF ) MIN = Math.min(MIN, HALF - black)
-    }
-
-    if ( white < black ) {
-      if ( white > HALF ) MIN = Math.min(MIN, white - HALF)
-      if ( white < HALF ) MIN = Math.min(MIN, HALF - white)
-    }
-    
-    // if( black === white ) return console.log(0);
+for(let h = 0 ; h <= L - 8 ; h++) {
+  for( let w = 0; w <= R - 8 ; w++) {
+    MIN = Math.min(MIN, compare(h,w));
   }
 }
 console.log(MIN);
+
+
+
 
 
