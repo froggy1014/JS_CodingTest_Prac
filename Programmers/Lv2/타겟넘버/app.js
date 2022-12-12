a7ede24bc5de0a2a1f204dd6b0900a2699cf60e5
froggy1014 <clcl6084@gmail.com>
2022-12-12 23:47:50 +0900

    Programmerse[타겟넘버]: 문제 풀이

---
+++ Programmers/Lv2/타겟넘버/app.js
@@ -0,0 +1,22 @@
+function solution(numbers, target) {
+  let answer = 0;
+  const length = numbers.length;
+
+  function dfs(count, sum) {
+    if (count === length) {
+      if (target === sum) {
+        answer++;
+      }
+      return;
+    }
+
+    dfs(count + 1, sum + numbers[count]);
+    dfs(count + 1, sum - numbers[count]);
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
+
+console.log(solution([4,1,2,1], 4))
